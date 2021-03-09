6b1186b7e191d6957e82abbf2aa8fdb505caa33f
almirars <almirasabitaaa@gmail.com>
2021-03-09 16:20:09 +0700

    tambah file

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,146 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+// import 'package:konversi_satuan/history.dart';
+import 'input.dart';
+import 'convert.dart';
+import 'result.dart';
+import 'textForm.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class _MyAppState extends State<MyApp> {
+  TextEditingController satuan = new TextEditingController();
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  double _inputSatuan = 0;
+  double _result = 0;
+  final inputController = TextEditingController();
+  List<String> listItem = ["Kg", "Hg", "Dag", "G", "Dg", "Cg", "Mg"];
+  List<String> listItem1 = ["Kg", "Hg", "Dag", "G", "Dg", "Cg", "Mg"];
+  List<String> listViewItem = List<String>();
+  List<String> listViewItem1 = List<String>();
+  String _newValue = "Kg";
+  String _newValue1 = "Kg";
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void hitung() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _inputSatuan = double.parse(satuan.text);
+      if (_newValue == "Kg")
+        _result = _inputSatuan * 10;
+      else if (_newValue == "Hg")
+        _result = _inputSatuan * 100;
+      else if (_newValue == "Dag")
+        _result = _inputSatuan * 1000;
+      else if (_newValue == "G")
+        _result = _inputSatuan * 10000;
+      else if (_newValue == "Dg")
+        _result = _inputSatuan * 100000;
+      else if (_newValue == "Cg")
+        _result = _inputSatuan * 1000000;
+      else
+        _result = (_inputSatuan * 10000000);
+      listViewItem.add("$_newValue" ":" "$_result");
+    });
+  }
+
+  void hitungLagi() {
+    setState(() {
+      _inputSatuan = double.parse(satuan.text);
+      if (_newValue == "Kg")
+        _result = _inputSatuan / 10;
+      else if (_newValue == "Hg")
+        _result = _inputSatuan / 100;
+      else if (_newValue == "Dag")
+        _result = _inputSatuan / 1000;
+      else if (_newValue == "G")
+        _result = _inputSatuan / 10000;
+      else if (_newValue == "Dg")
+        _result = _inputSatuan / 100000;
+      else if (_newValue == "Cg")
+        _result = _inputSatuan / 1000000;
+      else
+        _result = (_inputSatuan / 10000000);
+      listViewItem1.add("$_newValue1" ":" "$_result");
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Konverter Satuan"),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(8),
+          child: Row(
+            children: <Widget>[
+              textForm(inputController: satuan),
+              Text(
+                "Dari ",
+                style: TextStyle(fontSize: 30),
+              ),
+              DropdownButton<String>(
+                items: listItem.map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                value: _newValue,
+                onChanged: (String changeValue) {
+                  setState(() {
+                    _newValue = changeValue;
+                    // hitung();
+                  });
+                },
+              ),
+              Text(
+                "Ke ",
+                style: TextStyle(fontSize: 30),
+              ),
+              DropdownButton<String>(
+                items: listItem1.map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                value: _newValue1,
+                onChanged: (String changeValue) {
+                  setState(() {
+                    _newValue1 = changeValue;
+                    // hitungLagi();
+                  });
+                },
+              ),
+              Result(result: _result),
+              Convert(convertHandler: hitung),
+              Convert(convertHandler: hitungLagi),
+              // Container(
+              //   margin: EdgeInsets.only(top: 10, bottom: 10),
+              //   child: Text(
+              //     "Riwayat Konversi",
+              //     style: TextStyle(fontSize: 20),
+              //   ),
+              // ),
+              // history(listViewItem: listViewItem, listViewItem1: listViewItem1),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
